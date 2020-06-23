1b7973dde7d6e0b57f9f9e3d287f48b4994e0389
Shannon Sagehorn <srsagehorn@gmail.com>
2020-06-23 18:16:25 -0500

    fixed wrong soundtrack bug

--- script.js
+++ script.js
@@ -1,64 +1,8 @@
-// Shannon S.
-// Scroll down for Michael's portion
-
 $("button").on("click", function (event) {
   event.preventDefault();
-  $(".title").text("");
-  $("#vid").text("");
-  $("#songs").text("");
-  var apiKey = "AIzaSyDdGdXoEqADclmem8-3rtqWYNqdFpxvOnQ";
-
   var input = $(".searcharea").val();
 
-  var playlistIdURL =
-    "https://www.googleapis.com/youtube/v3/search?order=viewcount&part=snippet&q=" +
-    input +
-    "movie%20soundtrack&type=playlist&key=" +
-    apiKey;
-
-  $.ajax({
-    url: playlistIdURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var playlistId = response.items[0].id.playlistId;
-    $(".title").append(response.items[0].snippet.title);
-
-    var vidURL =
-      "https://www.googleapis.com/youtube/v3/playlists?part=player&part=contentDetails&id=" +
-      playlistId +
-      "&key=" +
-      apiKey;
-    // &part=snippet
-    $.ajax({
-      url: vidURL,
-      method: "GET",
-    }).then(function (playlistVid) {
-      $("#vid").append(playlistVid.items[0].player.embedHtml);
-      var playlistLength = playlistVid.items[0].contentDetails.itemCount;
-    });
-
-    var songListURL =
-      "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&part=contentDetails&playlistId=" +
-      playlistId +
-      "&key=" +
-      apiKey;
-
-    $.ajax({
-      url: songListURL,
-      method: "GET",
-    }).then(function (playlistResponse) {
-      var length = playlistResponse.pageInfo.totalResults;
-      for (var i = 0; i < length; i++) {
-        $("#songs").append(
-          "<li>" + playlistResponse.items[i].snippet.title + "</li>"
-        );
-      }
-      console.log(playlistResponse);
-    });
-  });
-
-  // Michael Z.
+  // Michael Z. (scroll down for Shannon's portion)
   $("#movie-result").text("");
   $("#movie-pic").text("");
   $(".mainTitle").text("");
@@ -86,5 +30,60 @@ $("button").on("click", function (event) {
     $("#actors").append("Actors: " + response.Actors);
     $("#rating").append("Rated: " + response.Rated);
     $("#length").append("Length:" + response.Runtime);
+
+    // Shannon S.
+
+    $(".title").text("");
+    $("#vid").text("");
+    $("#songs").text("");
+    var apiKey = "AIzaSyDdGdXoEqADclmem8-3rtqWYNqdFpxvOnQ";
+
+    var playlistIdURL =
+      "https://www.googleapis.com/youtube/v3/search?order=viewcount&part=snippet&q=" +
+      input +
+      response.Year +
+      "movie%20soundtrack&type=playlist&key=" +
+      apiKey;
+
+    $.ajax({
+      url: playlistIdURL,
+      method: "GET",
+    }).then(function (idResponse) {
+      console.log(idResponse);
+      var playlistId = idResponse.items[0].id.playlistId;
+      $(".title").append(idResponse.items[0].snippet.title);
+
+      var vidURL =
+        "https://www.googleapis.com/youtube/v3/playlists?part=player&id=" +
+        playlistId +
+        "&key=" +
+        apiKey;
+      // &part=snippet
+      $.ajax({
+        url: vidURL,
+        method: "GET",
+      }).then(function (playlistVid) {
+        $("#vid").append(playlistVid.items[0].player.embedHtml);
+      });
+
+      var songListURL =
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&part=contentDetails&playlistId=" +
+        playlistId +
+        "&key=" +
+        apiKey;
+
+      $.ajax({
+        url: songListURL,
+        method: "GET",
+      }).then(function (playlistResponse) {
+        var length = playlistResponse.pageInfo.totalResults;
+        for (var i = 0; i < length; i++) {
+          $("#songs").append(
+            "<li>" + playlistResponse.items[i].snippet.title + "</li>"
+          );
+        }
+        console.log(playlistResponse);
+      });
+    });
   });
 });
